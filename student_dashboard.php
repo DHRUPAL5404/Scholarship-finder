c0e516b7a68ecca031a2153e2c07279c832bc9be
Dhruti <movaliyadhruti3@gmail.com>
2026-01-30 11:52:43 +0530

    dashboard modified

--- student_dashboard.php
+++ student_dashboard.php
@@ -191,7 +191,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Dashboard - ScholarMatch</title>
-    <link rel="stylesheet" href="dashboard.css">
+   
 </head>
 <body>
 
