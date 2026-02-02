0e51999840aca04b2ebbd4d1e9a6518b801179bb
Dhruti <movaliyadhruti3@gmail.com>
2026-02-02 23:56:34 +0530

    Added main pages, removed dashboard, and updated logout

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ScholarMatch | Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+<body>
+
+    <header>
+        <h1>ScholarMatch</h1>
+        <nav>
+            <a href="login.php">Login</a> |
+            <a href="register.php">Register</a> |
+            <a href="about.html">About</a> |
+            <a href="contact.html">Contact</a>
+        </nav>
+        <hr>
+    </header>
+
+    <main>
+        <h2>Welcome to ScholarMatch</h2>
+        <p>
+            One platform where students register, login, complete profiles, and instantly check scholarship eligibility
+        </p>
+
+        <h3>Get Started</h3>
+        <ul>
+            <li><a href="register.php">New Student Registration</a></li>
+            <li><a href="login.php">Student / Admin Login</a></li>
+           
+        </ul>
+    </main>
+
+    <hr>
+
+    <footer>
+        <p>© 2026 ScholarMatch</p>
+    </footer>
+
+</body>
+</html>
